9a8db30e2fe3fe0b24406c5b17261137f84968a2
Saurav Suman <108250158+SauravRai18@users.noreply.github.com>
2026-05-21 22:17:07 +0530

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.cpp
@@ -0,0 +1,31 @@
+class Solution {
+   public:
+    bool hasDuplicate(vector<int>& nums) {
+        int n = nums.size();
+
+        /*        for (int i = 0; i < n; i++) {
+                    for (int j = i + 1; j < n; j++) {
+                        if (nums[i] == nums[j]) {
+                            return true;
+                        }
+                    }
+                }
+
+                return false;
+            }
+            */
+
+        unordered_set<int> st;
+
+        for (int i = 0; i < n; i++) {
+            if (st.find(nums[i]) != st.end()) {
+                return true;
+            }
+
+            st.insert(nums[i]);
+        }
+
+         return false;
+
+        }   
+    };
